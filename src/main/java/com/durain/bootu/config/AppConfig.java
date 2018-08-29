60c71b5e0425bcbf3d3950d96ddc5eb6cf3038de
hankorion <hankzhang.orion@gmail.com>
2018-08-29 19:14:06 +0800

    Init i18n for multi language

---
+++ src/main/java/com/durain/bootu/config/AppConfig.java
@@ -0,0 +1,32 @@
+package com.durain.bootu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:i18n/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource());
+		return bean;
+	}
+
+	@Override
+	public Validator getValidator() {
+		return validator();
+	}
+}
--- src/main/java/com/durain/bootu/controller/GameController.java
+++ src/main/java/com/durain/bootu/controller/GameController.java
@@ -1,6 +1,9 @@
 package com.durain.bootu.controller;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +18,9 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 
+	@Autowired
+	private MessageSource messageSource;
+
 	@PostMapping(value = "/game/add", produces = { "application/json;charset=UTF-8" })
 	public int addGame(Game game) {
 		return gameService.addGame(game);
@@ -23,5 +29,10 @@ public class GameController {
 	@GetMapping(value = "/game/all/{pageNum}/{pageSize}", produces = { "application/json;charset=UTF-8" })
 	public Object findAllGames(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize) {
 		return gameService.findAllGames(pageNum, pageSize);
+	}
+
+	@GetMapping("/game/welcome")
+	public String welcome(Locale locale) {
+		return messageSource.getMessage("welcome.message", null, locale);
 	}
 }
