c99a24b2a9d551ea9178a1a57868d936b905f9e9
hankorion <hankzhang.orion@gmail.com>
2018-08-29 16:42:53 +0800

    Add service, update springboot to 2.0.3 as 2.0.4 rest api return 404

--- src/main/java/com/durain/bootu/BootuApplication.java
+++ src/main/java/com/durain/bootu/BootuApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("com.durain.bootu.mapper")
 public class BootuApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/durain/bootu/controller/GameController.java
+++ src/main/java/com/durain/bootu/controller/GameController.java
@@ -1,31 +1,27 @@
 package com.durain.bootu.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.durain.bootu.model.Game;
 import com.durain.bootu.service.GameService;
 
-@Controller
-@RequestMapping(value = "/game")
+@RestController
 public class GameController {
-	
+
 	@Autowired
 	private GameService gameService;
-	
-	@ResponseBody
-	@RequestMapping(value = "/add", produces = {"application/json;charset=UTF-8"})
+
+	@PostMapping(value = "/game/add", produces = { "application/json;charset=UTF-8" })
 	public int addGame(Game game) {
-		 return gameService.addGame(game);
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/all/{pageNum}/{pageSize}", produces = {"application/json;charset=UTF-8"})
-	public Object findAllGames(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize){
-		return gameService.findAllGames(pageNum, pageSize);
+		return gameService.addGame(game);
 	}
 
+	@GetMapping(value = "/game/all/{pageNum}/{pageSize}", produces = { "application/json;charset=UTF-8" })
+	public Object findAllGames(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize) {
+		return gameService.findAllGames(pageNum, pageSize);
+	}
 }
