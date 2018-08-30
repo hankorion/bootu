71885f77f53dc60b957c9559777912cea5a9976b
hankorion <hankzhang.orion@gmail.com>
2018-08-30 10:33:58 +0800

    change web mvc configurer

--- src/main/java/com/durain/bootu/config/AppConfig.java
+++ src/main/java/com/durain/bootu/config/AppConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig extends WebMvcConfigurerAdapter {
+public class AppConfig implements WebMvcConfigurer {
 
 	@Bean
 	public ReloadableResourceBundleMessageSource messageSource() {
